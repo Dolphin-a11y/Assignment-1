88e6cf4ffb55dbcfb889b9159b0b1a520c230957
Dolphin-a11y <dolphin-a11y@users.noreply.github.com>
2026-07-17 13:18:42 +0700

    Use playable 3D chess projection

--- scripts/export-static.mjs
+++ scripts/export-static.mjs
@@ -26,6 +26,6 @@ html = html.replace("</head>", '<link rel="stylesheet" href="/bemuse.css?v=1"></
 html = html.replace("</head>", '<link rel="stylesheet" href="/custom-rhythm-embed.css?v=1"></head>');
 html = html.replace("</head>", '<link rel="stylesheet" href="/jigsaw-embed.css?v=1"></head>');
 html = html.replace("</head>", '<link rel="stylesheet" href="/chess-embed.css?v=1"></head>');
-html = html.replace("</body>", '<script src="/standalone.js?v=chess-hit-layer-v37" defer></script></body>');
+html = html.replace("</body>", '<script src="/standalone.js?v=chess-playable-3d-v38" defer></script></body>');
 await writeFile(new URL("../public/index.html", import.meta.url), html, "utf8");
 console.log("Exported public/index.html");
